0d109a2a8dc6517568b38f1a16c6c39624996ab6
DHIYAN51 <dhiyaneshkannan.r@indusnet.co.in>
2023-02-04 01:03:50 +0530

    varahi connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import UpdateUser from "./Pages/UpdateUser";
+import CreateUser from "./Pages/CreateUser";
+import Navigation from "./Components/Navbar";
 function App() {
   return (
-    <div className="App">
-       <h1>Varahi amman</h1>
-    </div>
+    <BrowserRouter>
+    <Navigation/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/updateform" element={<UpdateUser/>}/>
+        <Route path="/createform" element={<CreateUser/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/CreateUser.js
@@ -0,0 +1,43 @@
+import React,{useState}from "react";
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Axios from "axios";
+const CreateUser = () => {
+  const vercel = "https://back-end-task-leli.vercel.app";
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    // console.log(username + " "+ password);
+    Axios.post(`${vercel}/users/add`,{username:username, password:password}).then(()=>alert("User Created")).catch(e=>console.log(e))
+    setUsername("");
+    setPassword("");
+  }
+  return (
+    <Container>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>User name</Form.Label>
+          <Form.Control type="text" placeholder="Enter username"  onChange={(e)=>setUsername(e.target.value)} value={username}/>
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password"  onChange={(e)=>setPassword(e.target.value)} value={password}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default CreateUser;
