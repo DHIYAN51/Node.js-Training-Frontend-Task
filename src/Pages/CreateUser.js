cb201d0165457b995a5a5b6b2a6d8558ede57cea
DHIYAN51 <dhiyaneshkannan.r@indusnet.co.in>
2023-02-04 02:38:10 +0530

    finish

--- src/Pages/CreateUser.js
+++ src/Pages/CreateUser.js
@@ -3,7 +3,9 @@ import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Axios from "axios";
+import { useNavigate } from "react-router-dom";
 const CreateUser = () => {
+  const navigate = useNavigate();
   const vercel = "https://back-end-task-leli.vercel.app";
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -13,6 +15,7 @@ const CreateUser = () => {
     Axios.post(`${vercel}/users/add`,{username:username, password:password}).then(()=>alert("User Created")).catch(e=>console.log(e))
     setUsername("");
     setPassword("");
+    navigate('/')
   }
   return (
     <Container>
